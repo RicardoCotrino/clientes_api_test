ccf59a885d1401d667298874d0685c46db0a1b6e
ricardo.cotrino@gmail.com <x4254642S>
2021-12-04 16:57:03 -0300

    Incluir POST e TODO para os métodos restantes

--- src/test/java/ClientsTest.java
+++ src/test/java/ClientsTest.java
@@ -1,4 +1,5 @@
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.hamcrest.Matchers.*;
@@ -25,7 +26,7 @@ public class ClientsTest {
            @Test
            void testListaUnicoCliente_ClienteEhListadoComTodasAsInformacoes() {
 
-            int id = 4;
+            int id = 16;
                RestAssured.given()//Dado - Given
                        .log().all()  // loga toda a request - solicitação (entrada) - mostra o cabeçalho da solicitação
                        .when() //Quando - When - Request
@@ -36,6 +37,30 @@ public class ClientsTest {
                        .body("id",is(id));
 
            }
+
+    @Test
+    void testIncluiCliente_ClienteEhIncluidoComSucesso() {
+
+        String email = "fulana@teste.com";
+        String name = "Fulana";
+
+        RestAssured.given()//Dado - Given - Pré-condição
+                .body("{\n" +
+                        "  \"email\": \"" + email +"\",\n" +
+                        "  \"name\": \"" + name + "\"\n" +
+                        "}")
+                .log().all()  // loga toda a request - solicitação (entrada) - mostra o cabeçalho da solicitação
+                .contentType(ContentType.JSON)
+                .when() //Quando - When - Request
+                .post() // Faz a solicitação em si, método/verbo GET passando como parametro a ur/uri
+                .then() //Então - REsponse
+                .log().all()// loga toda a response
+                .statusCode(201); // verifica se o resultado da request é HTTP 200 OK
+
+
+    }
+
+    //1TODO fazer testes para os verbos DELETE e PUT
     }
 
 
